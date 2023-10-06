2f34916e3f1ac39a0e15dccc542912472e3a5672
jh71171 <144635658+jh71171@users.noreply.github.com>
2023-10-06 09:24:18 +0900

    2023.10.06

---
+++ php/board/board.php
@@ -0,0 +1,209 @@
+<?php
+    include "../connect/connect.php";
+    include "../connect/session.php";
+
+    // echo"<pre>";
+    // var_dump($_SESSION);
+    // echo"</pre>";
+
+     // 총 페이지 갯수
+     $sql = "SELECT count(boardID) FROM board";
+     $result = $connect -> query($sql);
+ 
+     $boardTotalCount = $result -> fetch_array(MYSQLI_ASSOC);
+     $boardTotalCount = $boardTotalCount['count(boardID)'];
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 블로그 만들기</title>
+
+    <!-- CSS -->
+    <?php include "../include/head.php"?>
+</head>
+<body class="gray">
+    <?php include "../include/skip.php"?>
+    <!-- //skip -->
+
+    <?php include "../include/header.php"?>
+    <!-- //header -->
+
+    <main id="main" role="main">
+        <div class="intro__inner bmStyle container">
+            <div class="intro__img">
+                <img srcset="../asset/img/intro02-min.jpg 1x, ../asset/img/intro02@2x-min.jpg 2x, ../asset/img/intro02@3x-min.jpg 3x" alt="인트로 이미지">
+            </div>
+            <div class="intro__text">
+                <h2>게시판</h2>
+                <p>
+                    웹디자이너, 웹 퍼블리셔, 프론트앤드 개발자를 위한 게시판입니다.<br>
+                    게시글 작성은 여기서 해주세요!
+                </p>
+            </div>
+        </div>
+        <section class="board__inner container">
+            <div class="board__search">
+                    <div class="left">
+                        * 총<em><?=$boardTotalCount?></em>건의 게시물이 등록 되어있습니다.
+                    </div>
+                    <div class="right">
+                        <form action="boardSearch.php" name="boardSearch" method="get">
+                            <fieldset>
+                                <legend class="blind">게시판 검색 영역</legend>
+                                <input type="search" name="searchKeyword" id="searchKeyword" placeholder="검색어를 입력하세요!">
+                                <select name="searchOption" id="searchOption">
+                                    <option value="title">제목</option>
+                                    <option value="content">내용</option>
+                                    <option value="name">등록자</option>
+                                </select>
+                                <button type="submit" class="btn__style3 white">검색</button>
+                                <a href="boardWrite.php" class="btn__style3">글쓰기</a>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+        <div class="board__table">
+                <table>
+                    <colgroup>
+                        <col style="width: 5%;">
+                        <col>
+                        <col style="width: 10%;">
+                        <col style="width: 15%;">
+                        <col style="width: 7%;">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>등록자</th>
+                            <th>등록일</th>
+                            <th>조회수</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+<?php
+
+    if(isset($_GET['page'])){
+        $page = (int) $_GET['page'];
+    } else {
+        $page = 1;
+    }
+
+    $viewNum = 10;
+    $viewLimit = ($viewNum * $page) - $viewNum;
+
+    //1~10 LIMIT 0, 10  -> page1 ($viewNum * 1) - $viewNum 
+    //11~20 LIMIT 10, 10 -> page2 ($viewNum * 2) - $viewNum 
+    //21~30 LIMIT 20, 10 -> page3 ($viewNum * 3) - $viewNum 
+    //31~40 LIMIT 30, 10 -> page4( $viewNum * 4) - $viewNum 
+
+    $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM board b JOIN members m ON(b.memberID = m.memberID) ORDER BY boardID DESC LIMIT {$viewLimit}, {$viewNum}";
+    $result = $connect -> query($sql);
+
+    if($result){
+        $count = $result -> num_rows;
+
+        if($count > 0){
+            for($i=0; $i<$count; $i++){
+                $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+                echo "<tr>";
+                echo "<td>".$info['boardID']."</td>";
+                echo "<td>".$info['boardTitle']."</td>";
+                echo "<td>".$info['youName']."</td>";
+                echo "<td>".date('Y-m-d' , $info['regTime'])."</td>";
+                echo "<td>".$info['boardView']."</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>게시글이없습니다.</td></tr>";
+        }
+    } else {
+        echo "관리자에게 문의해주세요.";
+    }
+?>
+
+                    </tbody>
+                </table>
+            </div>
+            <div class="board__pages">
+                <ul>
+<?php
+    // 총페이지 갯수
+    $boardTotalCount = ceil($boardTotalCount/$viewNum);
+
+    // 1 2 3 4 5 6 [7] 8 9 10
+    $pageView = 5;
+    $startPage = $page - $pageView;
+    $endPage = $page + $pageView;
+
+    // 이전과 처음으로 가는 페이지 계산식
+    $prevPage = $page - 1;
+    $firstPage = 1;
+
+    // 이전 페이지로 가는 링크
+    if ($prevPage >= 1) {
+        echo "<li class='prev'><a href='board.php?page={$prevPage}'>이전</a></li>";
+    }
+
+    // 처음 페이지로 가는 링크
+    if ($page > 1) {
+        echo "<li class='first'><a href='board.php?page={$firstPage}'>처음으로</a></li>";
+    }
+    
+
+    // 처음 페이지 초기화 / 마지막 페이지 초기화
+    if($startPage < 1) $startPage = 1;
+    if($endPage >= $boardTotalCount) $endPage = $boardTotalCount;
+
+    // 현재 보고있는 페이지에 active 붙이기
+    for($i=$startPage; $i <= $endPage; $i++){
+        $active = "";
+        if($i == $page) $active = "active";
+
+        echo "<li class='{$active}'><a href='board.php?page={$i}'>${i}</a></li>";
+    }
+
+  
+
+    // 이전 처음으로 가기 
+
+     // 다음 페이지로 가는 계산식
+     $nextPage = $page + 1;
+
+    // 다음 페이지로 가는 링크
+     if ($nextPage <= $boardTotalCount) {
+         echo "<li class='next'><a href='board.php?page={$nextPage}'>다음</a></li>";
+     }
+ 
+     // 마지막 페이지로 가는 링크
+     if ($page < $boardTotalCount) {
+         echo "<li class='last'><a href='board.php?page={$boardTotalCount}'>마지막으로</a></li>";
+     }
+
+
+?>
+                    <!-- <li class="first"><a href="#">처음으로</a></li>
+                    <li class="prev"><a href="#">이전</a></li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                    <li><a href="#">7</a></li>
+                    <li class="next"><a href="#">다음</a></li>
+                    <li class="last"><a href="#">마지막으로</a></li> -->
+                </ul>
+            </div>
+        </section>
+    </main>
+    <!-- //main -->
+
+    <?php include "../include/footer.php"?>
+    <!-- //footer -->
+</body>
+</html>
---
+++ php/board/boardWriteSave.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    include "../connect/connect.php";
+    include "../connect/session.php";
+
+    $boardTitle = $_POST['boardTitle'];
+    $boardContents = $_POST['boardContents'];
+    $boardView = 1;
+    $regTime = time();
+    $memberID = $_SESSION['memberID'];
+
+    // 제목이나 컨텐츠 내용이 없으면 등록이 안되어야하는 기능.
+
+   // 세션을 통해 사용자가 로그인이 되어 있는지 확인
+   if (!isset($_SESSION['memberID'])) {
+    // 사용자가 로그인되지 않은 경우, 로그인 페이지로 리디렉션 또는 에러 메시지를 표시할 수 있습니다.
+    echo "<script>alert('로그인 후에 게시글을 작성할 수 있습니다.'); window.history.back()</script>";
+} else {
+    // 데이터 입력 여부 확인
+    if(empty($boardTitle) || empty($boardContent)) {
+        echo "<script>alert('제목과 내용을 모두 입력해야 합니다.'); window.history.back()</script>";
+    } else {
+        $boardTitle = $connect -> real_escape_string($boardTitle);
+        $boardContents = $connect -> real_escape_string($boardContents);
+        $sql = "INSERT INTO board(memberID, boardTitle, boardContents, boardView, regTime) VALUES('$memberID', '$boardTitle', '$boardContents', '$boardView', '$regTime')";
+
+        $connect -> query($sql);
+        // if ($connect->query($sql)) {
+            // 글 작성이 성공한 경우 메시지를 표시하고 board.php로 리디렉션합니다.
+        echo "<script>alert('글이 성공적으로 작성되었습니다.'); window.location.href = 'board.php';</script>";
+    }
+} 
+
+?>
+</body>
+</html>
